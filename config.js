0acfd4271dfb6ce757b8432b569f4f48493fd07f
NielsE02 <71512277+NielsE02@users.noreply.github.com>
2026-08-02 22:02:22 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BINGO_CONFIG = {
-  SUPABASE_URL: "https://JOUW-PROJECT.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "JOUW-PUBLISHABLE-OF-ANON-KEY"
+  SUPABASE_URL: "https://zdneoaaoltngpynpkxyo.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_IZoLd5Htnt3wsJB0oVynjw_iNzZj_U9"
 };
